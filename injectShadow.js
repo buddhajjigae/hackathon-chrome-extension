09c1188f698f3d43c7dac1d3e7634f3f16a44d6f
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2021-03-11 21:24:19 -0500

    Update injectShadow.js

--- injectShadow.js
+++ injectShadow.js
@@ -1,11 +1,16 @@
 (function() {
-    document.documentElement.style.filter = "drop-shadow(16px 16px 20px red) invert(75%)";
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected drop-shadow!';
-
-	document.body.appendChild(div);
-	alert('Groovyness Enabled');
+    if (document.documentElement.style.filter === "none") { //|| document.documentElement.style.filter != "grayscale(100%)" || document.documentElement.style.filter != "contrast(200%)" || document.documentElement.style.filter != "hue-rotate(90deg)") {
+        document.documentElement.style.filter = "drop-shadow(16px 16px 20px red) invert(95%)";
+        var div = document.createElement('div');
+        div.style.position = 'fixed';
+        div.style.top = 0;
+        div.style.right = 0;
+        div.textContent = 'Injected drop-shadow';
+    
+        document.body.appendChild(div);
+        alert('Drop Shadow Applied');
+    } else {
+        document.documentElement.style.filter = "none";
+        alert('Drop Shadow Removed');
+    }
 })();
