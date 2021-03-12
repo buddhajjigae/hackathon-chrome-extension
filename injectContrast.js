f11f0f425b6cf10a587036aaa80d040732032505
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2021-03-11 21:23:49 -0500

    Update injectContrast.js

--- injectContrast.js
+++ injectContrast.js
@@ -1,11 +1,16 @@
 (function() {
-    document.documentElement.style.filter = "contrast(200%)";
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected contrast!';
-	
-	document.body.appendChild(div);
-	alert('High Contrast Enabled');
+    if (document.documentElement.style.filter != "contrast(200%)") {
+        document.documentElement.style.filter = "contrast(200%)";
+        var div = document.createElement('div');
+        div.style.position = 'fixed';
+        div.style.top = 0;
+        div.style.right = 0;
+        div.textContent = 'Injected high contrast';
+    
+        document.body.appendChild(div);
+        alert('High Contrast Applied');
+    } else {
+        document.documentElement.style.filter = "none";
+        alert('High Contrast Removed');
+    }
 })();
