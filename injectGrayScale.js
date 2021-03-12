be5f73b12f09e41f7f33b4df0a1b1733c3808520
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2021-03-11 21:23:59 -0500

    Update injectGrayScale.js

--- injectGrayScale.js
+++ injectGrayScale.js
@@ -1,11 +1,16 @@
 (function() {
-    document.documentElement.style.filter = "grayscale(100%)";
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected grayscale!';
-
-	document.body.appendChild(div);
-	alert('Grayscale Enabled');
+    if (document.documentElement.style.filter != "grayscale(100%)") {
+        document.documentElement.style.filter = "grayscale(100%)";
+        var div = document.createElement('div');
+        div.style.position = 'fixed';
+        div.style.top = 0;
+        div.style.right = 0;
+        div.textContent = 'Injected grayscale';
+    
+        document.body.appendChild(div);
+        alert('Grayscale Applied');
+    } else {
+        document.documentElement.style.filter = "none";
+        alert('Grayscale Removed');
+    }
 })();
