cf49bae70803a14eb2432b0521f4e040bed2b54d
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2021-03-11 21:24:12 -0500

    Update injectGroovy.js

--- injectGroovy.js
+++ injectGroovy.js
@@ -1,11 +1,16 @@
 (function() {
-    document.documentElement.style.filter = "hue-rotate(90deg)";
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected hue-rotate!';
-
-	document.body.appendChild(div);
-	alert('Groovyness Enabled');
+    if (document.documentElement.style.filter != "hue-rotate(90deg)") {
+        document.documentElement.style.filter = "hue-rotate(90deg)";
+        var div = document.createElement('div');
+        div.style.position = 'fixed';
+        div.style.top = 0;
+        div.style.right = 0;
+        div.textContent = 'Injected hue-rotate';
+    
+        document.body.appendChild(div);
+        alert('Hue Rotate Applied');
+    } else {
+        document.documentElement.style.filter = "none";
+        alert('Hue Rotate Removed');
+    }
 })();
